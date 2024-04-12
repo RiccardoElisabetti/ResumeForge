2fe211aa9870d2a8edf5df8ab6ee6123e50ae21c
Riccardo William <riccardo.elisabetti@fermilecce.edu.it>
2024-04-12 22:12:58 +0200

    charts and design templates added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import {Logo} from "./logo.tsx";
+import {GithubLogo} from "./github-logo.tsx"
 
 function App() {
-  return (
-    <h1>hello world</h1>
-  )
+	return (
+		<>
+			<header>
+				<div className="logo-wrapper">
+					<Logo></Logo>
+					<h2>ResumeForge</h2>
+				</div>
+				<div className="account">
+					<button className="log-in">log-in</button>
+					<button className="log-out">log-out</button>
+				</div>
+			</header>
+			<main>
+				<div className="hero">
+					<h1>
+						Semplice, veloce e efficace
+						<br></br>
+						<br></br>
+						Crea il tuo CV personalizzato in pochi passaggi
+					</h1>
+					<div className="hero-buttons">
+						<button>Crea il tuo CV adesso</button>
+					</div>
+				</div>
+				<div className="image"></div>
+			</main>
+			<footer>
+				<h2>Hai riscontrato problemi nella creazione del tuo CV?</h2>
+				<div className="github">
+					<GithubLogo></GithubLogo>
+					<div>github page</div>
+				</div>
+			</footer>
+		</>
+	);
 }
 
-export default App
+export default App;
